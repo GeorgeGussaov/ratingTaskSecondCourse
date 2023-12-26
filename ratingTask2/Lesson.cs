a137374d67b8fa1e5aaf9be23d5d10fd8402215d
Kasumyan2004@outlook.com <Kasumyan2004@outlook.com>
2023-12-26 12:12:36 +0300

    Добавьте файлы проекта.

---
+++ ratingTask2/Lesson.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ratingTask2
+{
+    public enum WeekDays 
+    {
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday, 
+        Sunday
+    }
+
+    public class Lesson
+    {
+        public string Label { get; set; }
+        public int Number { get; set; }
+        public string WeekNumber { get; set; }
+        public WeekDays Day { get; set; }
+        public string GroupNumber { get; set; }
+        public static char StaticLabelEmpty = (char)('A' - 1);
+        public Lesson()
+        {
+            StaticLabelEmpty++;
+            Label = StaticLabelEmpty.ToString();
+        }
+        public Lesson(string label, int number, int weekNumber, WeekDays day, string groupNumber)
+        {
+            Label = label;
+            Number = number;
+            if (weekNumber == 0) WeekNumber = "каждую неделю";
+            else if (weekNumber == 1) WeekNumber = "по первой неделе";
+            else if (weekNumber == 2) WeekNumber = "по второй неделе";
+            Day = day;
+            GroupNumber = groupNumber;
+        }
+        public override string ToString()
+        {
+            return $"{Label}, {Number} пара, {WeekNumber}, {Day}, {GroupNumber}";
+        }
+    }
+
+
+    public class Schedule : IEnumerable<Lesson>, IEnumerator<Lesson>
+    {
+        public string Name { get; set; }
+        public List<Lesson> listLesson = new List<Lesson>();
+        private int currentIndex = -1;
+        public Schedule(string name)
+        {
+            Name = name;
+        }
+        public void Add(Lesson les)
+        {
+            listLesson.Add(les);
+        }
+        public override string ToString()
+        {
+            return string.Join("\n", listLesson);
+        }
+
+        IEnumerator<Lesson> IEnumerable<Lesson>.GetEnumerator()
+        {
+            return this;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this;
+        }
+
+        public Schedule(List<Lesson> users)
+        {
+            this.listLesson = users;
+        }
+
+        public Lesson Current => listLesson[currentIndex];
+
+        object IEnumerator.Current => listLesson[currentIndex];
+
+        public bool MoveNext()
+        {
+            currentIndex++;
+            if (currentIndex >= listLesson.Count)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public void Reset()
+        {
+            currentIndex = -1;
+        }
+
+        public void Dispose()
+        { }
+
+    }
+
+
+}
---
+++ ratingTask2/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ratingTask2
+{
+    internal static class Program
+    {
+
+        static IEnumerable<Lesson> Sort(this Schedule sch)  //1 задание
+        {
+            //for (int i = sch.listLesson.Count - 1; i > 0; i--)
+            //{
+            //    for (int j = 0; j < i; j++)
+            //    {
+            //        int currentNumber = sch.listLesson[j].Number;
+            //        int nextNumber = sch.listLesson[j + 1].Number;
+            //        if (sch.listLesson[j].Number > sch.listLesson[j + 1].Number && sch.listLesson[j].Day > sch.listLesson[j + 1].Day)   
+            //        {
+            //            Lesson current = sch.listLesson[j];
+            //            sch.listLesson[j] = sch.listLesson[j + 1];        //при помощи сортировки пузырьком 
+            //            sch.listLesson[j + 1] = current;
+            //        }
+            //    }
+            //}
+            //return sch;
+            
+            return sch.OrderBy(x => x.Number);  //при помощи Linq. Конечно функция работает не совсем как в задании
+                                                //и возвращает перечисление Lessons а не Schedule, но я додумался только до такого :)
+                                                //или можно раскоментить решение пузырьком выше, но это совсем грамосткое решение.
+        }
+
+        //2 задание не успеваю, но идея вроде есть. При помощи компарера.
+
+        static void Main(string[] args)
+        {
+            Lesson les1 = new Lesson();
+            Lesson les2 = new Lesson();
+            Lesson les3 = new Lesson();
+            Lesson lesMat = new Lesson("Матан", 1, 0, WeekDays.Monday, "11");
+            Lesson lesAlg = new Lesson("Алгебра", 2, 1, WeekDays.Monday, "11");
+            Lesson lesAlgPract = new Lesson("Алгебра", 2, 1, WeekDays.Monday, "21");
+            //Console.WriteLine(lesMat);
+
+            Schedule schedule = new Schedule("ФМиКН");
+            schedule.Add(les3);
+            schedule.Add(les2);
+            schedule.Add(les1);
+            schedule.Add(lesAlg);
+            schedule.Add(lesAlgPract);
+            schedule.Add(lesMat);
+            Console.WriteLine(schedule);
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var newSchedule = schedule.Sort();
+            foreach(var item in newSchedule)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+
+    
+}
